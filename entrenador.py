54c3b303bfbe063a931354b84ae96e8937b1c1aa
jair <samuel3137119884@gmail.com>
2021-08-13 01:32:59 -0500

    v1.0.0.1

---
+++ entrenador.py
@@ -0,0 +1,44 @@
+import cv2 as cv 
+import os
+import numpy as np
+
+dataPath = 'bank_data'
+#Cambia a la ruta donde hayas almacenado Data
+peopleList = os.listdir(dataPath)
+print('Lista de personas: ', peopleList)
+
+labels = []
+facesData = []
+label = 0
+
+for nameDir in peopleList:
+	personPath = dataPath + '/' + nameDir
+	print('Leyendo las imágenes')
+
+	for fileName in os.listdir(personPath):
+		print('Rostros: ', nameDir + '/' + fileName)
+		labels.append(label)
+		facesData.append(cv.imread(personPath+'/'+fileName,0))
+		#image = cv.imread(personPath+'/'+fileName,0)
+		#cv.imshow('image',image)
+		#cv.waitKey(10)
+	label = label + 1
+
+#print('labels= ',labels)
+#print('Número de etiquetas 0: ',np.count_nonzero(np.array(labels)==0))
+#print('Número de etiquetas 1: ',np.count_nonzero(np.array(labels)==1))
+
+# Métodos para entrenar el reconocedor
+#face_recognizer = cv.face.EigenFaceRecognizer_create()
+#face_recognizer = cv.face.FisherFaceRecognizer_create()
+face_recognizer = cv.face.LBPHFaceRecognizer_create()
+
+# Entrenando el reconocedor de rostros
+print("Entrenando...")
+face_recognizer.train(facesData, np.array(labels))
+
+# Almacenando el modelo obtenido
+#face_recognizer.write('modeloEigenFace.xml')
+#face_recognizer.write('modeloFisherFace.xml')
+face_recognizer.write('modeloLBPHFace.xml')
+print("Modelo almacenado...")
